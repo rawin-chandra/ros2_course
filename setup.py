5bc8beffb5a0170a0ffc507ae0ccc53e3dc5da5e
rawin-chandra <40013619+rawin-chandra@users.noreply.github.com>
2024-09-02 16:27:51 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'motor_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ninja',
+    maintainer_email='ninja@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'motor_control_node = motor_control.motor:main',
+        ],
+    },
+)
